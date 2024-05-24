368ee16ef0cc748f2f54e025c94833afead68592
Kwok-Po Chu <kwchu@f0r4s14.codam.nl>
2024-05-24 13:27:33 +0200

    add save and load terminal screen functions

--- src/ttools_terminal.c
+++ src/ttools_terminal.c
@@ -6,7 +6,7 @@
 /*   By: kwchu <kwchu@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/23 19:22:21 by kwchu         #+#    #+#                 */
-/*   Updated: 2024/05/23 19:22:28 by kwchu         ########   odam.nl         */
+/*   Updated: 2024/05/24 13:20:28 by kwchu         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,17 @@
 void	clear_terminal_screen(void)
 {
 	printf("\033[2J");
+	fflush(stdout);
+}
+
+void	save_terminal_screen(void)
+{
+	printf("\033[?1049h");
+	fflush(stdout);
+}
+
+void	load_terminal_screen(void)
+{
+	printf("\033[?1049l");
 	fflush(stdout);
 }
